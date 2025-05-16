8a0d511e61c8a49dc3bf2f22d41205c27b5607de
Rob Dumas <robdumas@gmail.com>
2025-05-16 18:43:06 -0500

    Added droplet and volume attachment with Tofu

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -27,3 +27,26 @@ resource "digitalocean_volume" "tf_do_volume" {
   description              = "${var.item_prefix}-${var.item_label} volume"
   tags                     = ["${var.item_prefix}", "${var.item_label}"]
 }
+
+resource "digitalocean_droplet" "tf_do_droplet" {
+  name              = "${var.item_prefix}-${var.item_label}-${var.do_region}"
+  image             = var.do_image
+  size              = var.do_instance_type
+  region            = var.do_region
+  ssh_keys          = [var.do_ssh_keyid]
+  backups           = true
+  droplet_agent     = true
+  graceful_shutdown = true
+  ipv6              = true
+  monitoring        = true
+  vpc_uuid          = digitalocean_vpc.tf_do_vpc.id
+  depends_on = [
+    digitalocean_vpc.tf_do_vpc,
+    digitalocean_volume.tf_do_volume
+  ]
+}
+
+resource "digitalocean_volume_attachment" "do_volume_attach" {
+  droplet_id = digitalocean_droplet.tf_do_droplet.id
+  volume_id  = digitalocean_volume.tf_do_volume.id
+}
--- opentofu/outputs.tf
+++ opentofu/outputs.tf
@@ -1,8 +1,19 @@
+output "do_droplet_ipv4_address_public" {
+  description = "IPv4 address, external"
+  value       = digitalocean_droplet.tf_do_droplet.ipv4_address
+}
+
+output "do_droplet_ipv4_address_private" {
+  description = "IPv4 address, internal"
+  value       = digitalocean_droplet.tf_do_droplet.ipv4_address_private
+}
+
 output "do_volume_id" {
   description = "The UUID of the created volume"
   value       = digitalocean_volume.tf_do_volume.id
   sensitive   = false
 }
+
 output "do_vpc_id" {
   description = "The UUID of the created VPC"
   value       = digitalocean_vpc.tf_do_vpc.id
