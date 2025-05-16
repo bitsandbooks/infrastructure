8924263eec6a532bfacfddd2fe82790a355c6613
Rob Dumas <robdumas@gmail.com>
2025-05-16 18:43:06 -0500

    Added droplet and volume attachment with Tofu

--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,3 +1,25 @@
+variable "do_image" {
+  type        = string
+  description = <<EOT
+        DigitalOcean Images available. Send to list all available Images: curl -X GET \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer <your-digital-ocean-token>" \
+        "https://api.digitalocean.com/v2/images?page=1&per_page=100&type=distribution" | jq '.images[].slug'
+        EOT
+  default     = "debian-12-x64"
+}
+
+variable "do_instance_type" {
+  type        = string
+  description = <<EOT
+        DigitalOcean Droplets types to use. Send to list all available Droplets types: curl -X GET \
+        -H "Content-Type: application/json"
+        -H "Authorization: Bearer <your-digital-ocean-token>" \
+        "https://api.digitalocean.com/v2/sizes" | jq '.sizes[].slug'
+        EOT
+  default     = "s-1vcpu-2gb"
+}
+
 variable "do_region" {
   type        = string
   description = <<EOT
@@ -7,6 +29,12 @@ variable "do_region" {
         "https://api.digitalocean.com/v2/regions" | jq '.regions[].slug'
         EOT
   default     = "nyc1"
+}
+
+variable "do_ssh_keyid" {
+  description = "Root user SSH key"
+  type        = string
+  sensitive   = true
 }
 
 variable "do_token" {
